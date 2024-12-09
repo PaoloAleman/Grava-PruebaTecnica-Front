cca6f7da22b4702e327e04a7c02b298122704037
Paolo Aleman <paoloaleman86@gmail.com>
2024-12-08 22:00:17 -0300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,158 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import {useEffect, useState} from "react";
+
+type Category = {
+    type: { name: string };
+};
+
+type Pokemon = {
+    id: number;
+    name: string;
+    url: string;
+    image: string;
+    categories: Category[];
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [name, setName] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const fetchTenPokemons = async () => {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const data = await res.json();
+        return data.results;
+    };
+
+    const fetchPokemon = async (url: string) => {
+        const res = await fetch(url);
+        return await res.json();
+    };
+
+    const fetchData = async () => {
+        const pokemonList = await fetchTenPokemons();
+        return await Promise.all(
+            pokemonList.map(async (pokemon: Pokemon) => {
+                const details = await fetchPokemon(pokemon.url);
+                return {
+                    ...pokemon,
+                    image: details.sprites.other.dream_world.front_default || '',
+                    categories: details.types,
+                };
+            })
+        );
+    };
+
+    useEffect(() => {
+        fetchData().then(data => setPokemons(data));
+    }, []);
+
+    return (
+        <>
+            <header className={styles.header} id={"hero"}>
+                <img src={"./pokemon.svg"} alt={"Logo"} />
+                <nav>
+                    <ul>
+                        <a href={'#hero'}>Inicio</a>
+                        <a href={'#general'}>General</a>
+                        <a href={'#list'}>Listado</a>
+                        <a href={'#contact'}>Contacto</a>
+                    </ul>
+                </nav>
+            </header>
+
+            <main className={styles.main}>
+                <div className={styles.hero}>
+                    <img src={"./pokeball.svg"} alt={"Pokeball"} />
+                    <div>
+                        <h1>¡Bienvenido al mundo Pokémon!</h1>
+                        <h2>Explora y revive tu aventura favorita</h2>
+                        <p>
+                            Descubre todos tus Pokémon favoritos en un solo lugar. Nuestra
+                            plataforma te permite buscar, filtrar y aprender datos fascinantes
+                            sobre cada Pokémon.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.general} id={"general"}>
+                    <h1>General</h1>
+                    <p>
+                        Pokémon es una de las franquicias más icónicas del mundo del
+                        entretenimiento, reconocida por su amplio universo de criaturas y su
+                        impacto cultural a nivel global.
+                    </p>
+                    <div>
+                        <section>
+                            <h2>Creación</h2>
+                            <p>
+                                Fue creado en 1996 por Satoshi Tajiri y desarrollado por Game
+                                Freak para Nintendo. La primera entrega fue el videojuego
+                                *Pokémon Rojo y Verde* lanzado en Japón.
+                            </p>
+                        </section>
+                        <section>
+                            <h2>Difusión</h2>
+                            <p>
+                                Fue famoso en todo el mundo, alcanzando un gran éxito con su
+                                anime, cartas coleccionables, juguetes, y videojuegos que han
+                                vendido más de 400 millones de copias a nivel global.
+                            </p>
+                        </section>
+                        <section>
+                            <h2>Autor</h2>
+                            <p>
+                                Fue creado por Satoshi Tajiri, inspirado por su amor por la
+                                recolección de insectos durante su infancia. Tajiri diseñó el
+                                concepto de capturar y coleccionar criaturas fantásticas.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+
+                <div className={styles.list} id={"list"}>
+                    <h1>Pokemones</h1>
+                    <form>
+                        <label htmlFor="name">Nombre</label>
+                        <input type={"text"}
+                               onInput={(e) =>{
+                                   //@ts-ignore
+                                   setName(e.target.value)
+                               }}
+                               placeholder={"Ingrese un nombre!"}
+                               name={"name"} />
+                    </form>
+                    <div>
+                        {pokemons.filter(pokemon => name == "" || pokemon.name.includes(name.toLowerCase())).slice(0, 10).map((pokemon, index) => (
+                            <section key={index}>
+                                <div>
+                                    <img
+                                        src={pokemon.image}
+                                        alt={pokemon.name}
+                                    />
+                                </div>
+                                <div>
+                                    <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                                    <div>
+                                        <p>Tipos:
+                                            {pokemon.categories.map((category, idx) => (
+                                                <span key={idx}> {category.type.name.charAt(0).toUpperCase() + category.type.name.slice(1)}
+                                                    {idx < pokemon.categories.length - 1 ? ' | ' : ''}</span>
+                                            ))}
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </section>
+                        ))}
+                    </div>
+                </div>
+            </main>
+            <footer className={styles.footer} id={"contact"}>
+                <div>
+
+                </div>
+            </footer>
+        </>
+    );
 }
